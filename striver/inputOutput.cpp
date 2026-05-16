721b678a3e90594f83c7403ece18974d44a6941e
Tohibur <tohibur.tauhid@gmail.com>
2026-05-16 10:26:10 +0530

    Change directory

---
+++ striver/inputOutput.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a, b;
+
+    std::cin >> a;
+    std::cin >> b;
+
+    std::cout << "value of nothing: " << a << std::endl << "Value of b: " << b ;
+
+    return 0;
+}
